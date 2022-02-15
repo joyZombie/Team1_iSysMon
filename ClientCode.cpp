d5a3566a4e04f036e76ff67b1c00a58c3084136a
drago-123 <43999932+drago-123@users.noreply.github.com>
2022-02-15 14:46:55 +0530

    After binding the system data
    And sending it to server the server .

--- ClientCode.cpp
+++ ClientCode.cpp
@@ -1,14 +1,75 @@
 #include<iostream>
 #include<winsock.h>
+#include"si.h"
+#include<string.h>
 using namespace std;
 
 #define PORT 8080
 int nClientSocket;
 struct sockaddr_in srv;
+
+////////////////////////////////////////
+
+string storeData::getHostName()
+{ //Character array to store the host name
+	char szPath[128] = ""; //The WSADATA structure contains information about the Windows Sockets implementation.
+	WSADATA wsaData;// Creating a variable of type WSADATA ; //The WSAStartup function initiates use of the Winsock DLL by a process
+	WSAStartup(MAKEWORD(2, 2), &wsaData); //gethostname()----returns the name of the host processor that the program is running on
+	gethostname(szPath, sizeof(szPath));
+	return szPath;
+}
+int storeData::getTotalRAM()
+{
+	int ret = 0; //MEMORYSTAUSEX IS A STURUCTURE THAT Contains information about the current state of both physical and virtual memory, including extended memory.
+	MEMORYSTATUSEX m;
+	m.dwLength = sizeof(m);
+	GlobalMemoryStatusEx(&m);
+	ret = (int)(m.ullTotalPhys >> 20); return ret;
+}
+int storeData::getAvailRAM()
+{
+	int ret = 0;
+	MEMORYSTATUSEX m;
+	m.dwLength = sizeof(m);
+	GlobalMemoryStatusEx(&m);
+	ret = (int)(m.ullAvailPhys >> 20);
+	return ret;
+}
+void storeData::fetchData()
+{
+	hostName = getHostName();
+	totalRam = getTotalRAM();
+	availRam = getAvailRAM();
+}
+string storeData::stringify()
+{
+	string str;
+	str += (hostName)+", ";
+	str += to_string(totalRam) + "MB, ";
+	str += to_string(availRam) + "MB, "; return str;
+}
+/*void storeData::writeInFile()
+{
+	ofstream myFile("Stats.txt");
+	//Writing into the file
+	myFile << stringify() << endl << endl;
+	myFile.close();
+}
+*/
+
+
+
+
+
+
+
+
+///////////////////////////////////////
 int main()
 {
 	int nRet = 0;
-	WSADATA ws;	
+	WSADATA ws;
+	
 
 	if (WSAStartup(MAKEWORD(2, 2), &ws) < 0)
 	{
@@ -52,15 +113,23 @@ int main()
 		cout << endl << "Press something to see msg by server";
 		getchar();
 		cout << endl << buff;
-		cout << endl << "Now sending the new data to the server" <<endl;
+		cout << endl << "Now sending the new data to the server" << endl;
 		while (1)
 		{
-			fgets(buff, 256, stdin);
+			//fgets(buff, 256, stdin);
+			storeData obj;
+			obj.fetchData();
+
+			string s=obj.stringify();
+			for (int x = 0; x < s.length(); x++)
+			{
+				buff[x] = s[x];
+			}
 			send(nClientSocket, buff, 256, 0);
 			cout << endl << "Press any key to see the response from server";
 			getchar();
 			recv(nClientSocket, buff, 256, 0);
-			cout << endl << buff << endl << "Now send nxt iteration data"<<endl;
+			cout << endl << buff << endl << "Now send nxt iteration data" << endl;
 		}
 	}
 	return 0;
