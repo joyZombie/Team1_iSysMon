01b30d30f7e90728fa3c0ad7741c2e315e7622be
Praggyann <85470315+Praggyann@users.noreply.github.com>
2022-02-18 01:16:13 +0530

    Add files via upload

---
+++ Server_DataIntegrity/Header.h
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream> 
+#include<Lmcons.h>
+#include<Winsock2.h>
+#include<Windows.h>
+#include<Ws2tcpip.h>
+#include<tchar.h>
+#include<vector>
+using namespace std;
+
+void ProcessNewMessage(int nClientSocket);
+void ProcessNewRequest();
+
+class parseData {
+public:
+
+    parseData(
+        string hsN,
+        string usN,
+        int totRam,
+        int avRam,
+        float cpuL,
+        int prcType,
+        int nOP,
+        int cpuIT,
+        int chkSum
+        )
+    {
+        hstName = hsN;
+        usrName = usN;
+        totlRam = totRam;
+        avlblRam = avRam;
+        cpuLd = cpuL;
+        prcssType = prcType;
+        noOfPrc = nOP;
+        cpuIdTime = cpuIT;
+        checkS = chkSum;
+    }
+
+    void display() {
+        cout << "HostName: " << hstName << endl;
+        cout << "UserName: " << usrName << endl;
+        cout << "Total RAM: " << totlRam << " MB" << endl;
+        cout << "Avlbl RAM: " << avlblRam << " MB" << endl;
+        cout << "CPU Load: " << cpuLd << " %" << endl;
+        cout << "CPU Idle Time: " << cpuIdTime << " ms" << endl;
+        cout << "No. of Processors: " << noOfPrc << endl;
+        cout << "Processor Type: " << prcssType << endl;
+        cout << "CheckSum at client side: " << checkS << endl;
+    }
+
+    int checkData() {
+        int key = 101;
+        int cS = 0;
+        cS += (sizeof(hstName) ^ key);
+        cS += (sizeof(usrName) ^ key);
+        cS += (sizeof(totlRam) ^ key);
+        cS += (sizeof(avlblRam) ^ key);
+        cS += (sizeof(cpuLd) ^ key);
+        cS += (sizeof(cpuIdTime) ^ key);
+        cS += (sizeof(noOfPrc) ^ key);
+        cS += (sizeof(prcssType) ^ key);
+        return cS;
+    }
+
+    string hstName;
+    string usrName;
+    int totlRam;
+    int avlblRam;
+    float cpuLd;
+    int prcssArch;
+    int prcssType;
+    int noOfPrc;
+    int cpuIdTime;
+    int checkS;
+};
