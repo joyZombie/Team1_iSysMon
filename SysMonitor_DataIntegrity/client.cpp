60aa8938612c7ed7e9f6b94be96f22740176ce4a
Praggyann <85470315+Praggyann@users.noreply.github.com>
2022-02-24 19:33:20 +0530

    Update client.cpp

--- SysMonitor_DataIntegrity/client.cpp
+++ SysMonitor_DataIntegrity/client.cpp
@@ -2,20 +2,125 @@
 * ####IGNORE CODE BETWEEN "#if 0   #endif"
 */
 
-#include "sysInteraction.h"
+#include"sysinteract.h"
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-using namespace std;
-
+struct sockaddr_in srv;
 #define PORT 8080
 int nClientSocket;
-struct sockaddr_in srv;
 
-class SendData {
+char buff[5000] = { 0 };
 
-};
+void storeData::sendDataToServer(string uid)
+{
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+		string s = uid + "," + fetchNewData();
 
-void main()
+		//Parsing data at client side
+		string cuid;
+		string hsN;
+		string usN;
+		int totRam;
+		int avRam;
+		int totDiskSp;
+		int freeDiskSp;
+		float cpuL;
+		int cpuIT;
+		string prcArch;
+		int nOP;
+		int prcType;
+		string timeStmp;
+
+		int checkSum;
+
+		vector<parseData> data;
+		stringstream storeString(s);
+		string tempString;
+
+		string line = "";
+
+		while (getline(storeString, line)) {
+			stringstream inputString(line);
+
+			getline(inputString, cuid, ',');
+
+			getline(inputString, hsN, ',');
+
+			getline(inputString, usN, ',');
+
+			getline(inputString, tempString, ',');
+			totRam = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, tempString, ',');
+			avRam = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, tempString, ',');
+			totDiskSp = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, tempString, ',');
+			freeDiskSp = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, tempString, ',');
+			cpuL = atof(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, tempString, ',');
+			cpuIT = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, prcArch, ',');
+
+			getline(inputString, tempString, ',');
+			nOP = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, tempString, ',');
+			prcType = atoi(tempString.c_str());
+			tempString = "";
+
+			getline(inputString, timeStmp, ',');
+
+#if 0
+			//convert checkSum into string datatype
+			stringstream conv;
+			conv << checkSum;
+			conv >> chkSum;
+			//append checkSum at end of original data
+			arr.append(chkSum);
+#endif
+			parseData pD(cuid, hsN, usN, totRam, avRam, totDiskSp, freeDiskSp, cpuL, cpuIT, prcArch, nOP, prcType, timeStmp);
+
+			checkSum = pD.checkData();
+
+
+			//for multiple data streams
+			//data.push_back(pD);
+
+			line = "";
+
+
+		}
+
+		s = to_string(checkSum) + "," + s;
+		cout << s << endl;
+		cout << "checksum appended: " << checkSum << endl;
+
+		int x = 0;
+		for (; x < s.length(); x++)
+		{
+			buff[x] = s[x];
+		}
+		//buff[x] = '~';
+		send(nClientSocket, buff, 5000, 0);
+	}
+}
+
+int main()
 {
 	int nRet = 0;
 	WSADATA ws;
@@ -26,10 +131,7 @@ void main()
 		WSACleanup();
 		exit(EXIT_FAILURE);
 	}
-	else
-	{
-		cout << endl << "WSA is initialized.";
-	}
+
 	nClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (nClientSocket < 0)
 	{
@@ -39,13 +141,21 @@ void main()
 	}
 	else
 	{
-		cout << endl << "The Socket is opened successfully.";
+		cout << endl << "WSA is initialized AND The Socket is opened successfully.";
 	}
 	srv.sin_family = AF_INET;
 	srv.sin_port = htons(PORT);
-	//srv.sin_addr.s_addr = inet_addr("127.0.0.1");
-	InetPton(AF_INET, _T("127.0.0.1"), &srv.sin_addr.s_addr);
+	srv.sin_addr.s_addr = inet_addr("127.0.0.1");
 	memset(&(srv.sin_zero), 0, 8);
+
+	string uid;
+	cout << endl << "Enter the name of client :";
+	cin >> uid;
+	for (int i = 0; i < uid.length(); i++)
+	{
+		uid[i] = tolower(uid[i]);
+	}
+	cout << uid;
 
 	nRet = connect(nClientSocket, (struct sockaddr*)&srv, sizeof(srv));
 	if (nRet < 0)
@@ -56,178 +166,48 @@ void main()
 	}
 	else
 	{
-
-		cout << endl << "Connected to the srever";
-		char buff[255] = { 0 };
 		recv(nClientSocket, buff, 255, 0);
-		cout << endl << "Press something to see msg by server";
-		getchar();
 		cout << endl << buff;
-		cout << endl << "Now sending the new data to the server" << endl;
-		while (1)
+		cout << endl << "Connected to the server";
+		cout << endl << "Now sending the System Information to the server............" << endl;
+
+		storeData obj;
+		obj.intialiseFetchDataThread();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+		obj.intialiseSendDataThread(uid);
+
+		int input;
+
+		while (true)
 		{
-#if 0
-			storeData obj;
-			obj.fetchData();
-
-			string str = obj.stringify();
-#endif
-			
-#if 0
-			while (i != str.length())
+			cout << "Enter the input" << endl;
+			cin >> input;
+			char msg[5000] = { 0 };
+			switch (input)
 			{
-				buff[i] = str[i];
-				i++;
+			case 1:
+				cout << "I'm in case 1";
+				msg[0] = '1';
+				send(nClientSocket, msg, 5000, 0);
+				recv(nClientSocket, msg, 5000, 0);
+				cout << msg;
+				break;
+			case 2: cout << "I'm in case 2";
+				msg[0] = 2;
+				send(nClientSocket, msg, 255, 0);
+				recv(nClientSocket, msg, 255, 0);
+				break;
+			case 3: cout << "I'm in case 3";
+				msg[0] = 3;
+				send(nClientSocket, msg, 255, 0);
+				recv(nClientSocket, msg, 255, 0);
+				break;
+			default:
+				exit(0);
 			}
-#endif
-			int i = 0;
-			//fetching data
-			string arr;
-			
-			storeData obj;
-			obj.fetchData();
-			arr = obj.stringify();
-			
-
-			
-			//Parsing data at client side
-			string hsN;
-			string usN;
-			int totRam;
-			int avRam;
-			float cpuL;
-			//int cpuIT;
-			int nOP;
-			int prcType;			
-
-			vector<parseData> data;
-			stringstream storeString(arr);
-			string tempString;
-
-			string line = "";
-			string chkSum;
-
-			while (getline(storeString, line)) {
-				stringstream inputString(line);
-
-				getline(inputString, hsN, ',');
-				getline(inputString, usN, ',');
-
-				getline(inputString, tempString, ',');
-				totRam = atoi(tempString.c_str());
-				tempString = "";
-
-				getline(inputString, tempString, ',');
-				avRam = atoi(tempString.c_str());
-				tempString = "";
-
-				getline(inputString, tempString, ',');
-				cpuL = atof(tempString.c_str());
-				tempString = "";
-#if 0
-				getline(inputString, tempString, ',');
-				cpuIT = atoi(tempString.c_str());
-				tempString = "";
-#endif
-
-				getline(inputString, tempString, ',');
-				nOP = atoi(tempString.c_str());
-				tempString = "";
-
-				getline(inputString, tempString, ',');
-				prcType = atoi(tempString.c_str());
-				tempString = "";
-				
-
-				parseData pD(hsN, usN, totRam, avRam, cpuL, nOP, prcType); //cpuIT, prcType);
-
-				int checkSum = pD.checkData();
-				arr += to_string(checkSum);
-#if 0
-				//convert checkSum into string datatype
-				stringstream conv;
-				conv << checkSum;
-				conv >> chkSum;
-				//append checkSum at end of original data
-				arr.append(chkSum);
-#endif
-
-
-				//for multiple data streams
-				data.push_back(pD);
-
-				line = "";
-
-			}
-
-			
-			int temp = arr.length();
-
-			//storing final data
-			for (int j = 0; j < temp; j++)
-			{
-				buff[j] = arr[j];
-			}
-
-
-#if 0
-			stringstream ss(str);
-			string tempString;
-			string ram;
-			int age;
-
-			// Making the string empty
-			str = "";
-
-			// Running loop till end of stream
-			// and getting every word
-			string line = "";
-			while (getline(ss, line)) {
-				stringstream inputString(line);
-
-
-				getline(inputString, ram, ',');
-				getline(inputString, tempString, ',');
-				age = atoi(tempString.c_str());
-				line = "";
-		}
-#endif
-
-#if 0
-				string hsN;
-				string usN;
-				int totRam;
-				int avRam;
-				float cpuL;
-				int prcArch;
-				int prcType;
-				int nOP;
-				int cpuIT;
-				string tempString = "";
-
-				stringstream inputString;
-				line = "";
-#endif
-
-
-			
-#if 0
-
-			stringstream ss;
-			ss << temp;
-			string s;
-			ss >> s;
-
-#endif
-
-			//fgets(buff, 256, stdin);
-
-			send(nClientSocket, buff, sizeof(buff), 0);
-			cout << endl << "Press any key to see the response from server";
-			getchar();
-			recv(nClientSocket, buff, 256, 0);
-			cout << endl << buff << endl << "Now send nxt iteration data" << endl;
 		}
 	}
+	return 0;
 }
 
